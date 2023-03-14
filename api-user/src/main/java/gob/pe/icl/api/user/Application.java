eccaa930d66f4034f77ce2acfa2860b064fb0889
jofrantoba <chescot2302@gmail.com>
2023-03-14 10:35:35 -0500

    dependecys

--- api-user/src/main/java/gob/pe/icl/api/user/Application.java
+++ api-user/src/main/java/gob/pe/icl/api/user/Application.java
@@ -15,11 +15,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    @PostConstruct
-    public void init() {
-        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
