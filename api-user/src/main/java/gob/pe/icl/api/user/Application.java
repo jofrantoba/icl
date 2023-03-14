1d975d4ce29223fcf2780a00c4aa4a100b90cc7e
jofrantoba <chescot2302@gmail.com>
2023-03-14 10:18:52 -0500

    apí rest user

---
+++ api-user/src/main/java/gob/pe/icl/api/user/Application.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gob.pe.icl.api.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Usuario
+ */
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {        
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
