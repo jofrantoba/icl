f7ba1a71dc2ef13d4e95c1d61e3cea9c0c5a3fc2
jofrantoba <chescot2302@gmail.com>
2023-03-14 12:58:33 -0500

    fix resuelto de logs

--- api-car/src/main/java/gob/pe/icl/api/car/Application.java
+++ api-car/src/main/java/gob/pe/icl/api/car/Application.java
@@ -1,19 +1,20 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package gob.pe.icl.api.user;
+package gob.pe.icl.api.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  *
  * @author Usuario
  */
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
